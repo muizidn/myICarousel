2499d3a46c83c20cc7aa6c13fa79d3c9d684d9e3
muizidn <muiz.idn@gmail.com>
2020-10-17 09:38:45 +0700

    add impl to carousel

--- myICarousel/ViewController.m
+++ myICarousel/ViewController.m
@@ -10,33 +10,56 @@
 #import "iCarousel.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet iCarousel *iCarouselView;
+@end
+
+@interface ViewController (iCarouselDataSource) <iCarouselDataSource>
+@end
+
+@interface ViewController (iCarouselDelegate) <iCarouselDelegate>
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.view setBackgroundColor:UIColor.redColor];
+    [self.iCarouselView setDataSource:self];
+    [self.iCarouselView setDelegate:self];
+    [self.view setBackgroundColor:UIColor.brownColor];
 }
 
 
-@end
-
-@interface ViewController (iCarouselDataSource) <iCarouselDataSource>
 @end
 
 @implementation ViewController (iCarouselDataSource)
 - (nonnull UIView *)carousel:(nonnull iCarousel *)carousel viewForItemAtIndex:(NSInteger)index reusingView:(nullable UIView *)view {
-    return NULL;
+    UILabel * label = [[UILabel alloc]
+                       initWithFrame:
+                       CGRectMake(0, 0,
+                                  carousel.frame.size.width,
+                                  carousel.frame.size.height)];
+    [label setText:[NSString stringWithFormat:@"label %ld", (long)index]];
+    [label setTextAlignment:NSTextAlignmentCenter];
+    switch (index) {
+        case 0:
+            [label setBackgroundColor:UIColor.redColor];
+            break;
+        case 1:
+            [label setBackgroundColor:UIColor.blueColor];
+            break;
+        case 2:
+            [label setBackgroundColor:UIColor.yellowColor];
+            break;
+        default:
+            break;
+    }
+    return label;
 }
 
 - (NSInteger)numberOfItemsInCarousel:(nonnull iCarousel *)carousel {
-    return 0;
+    return 3;
 }
 
-@end
-
-@interface ViewController (iCarouselDelegate) <iCarouselDelegate>
 @end
 
 @implementation ViewController (iCarouselDelegate)
